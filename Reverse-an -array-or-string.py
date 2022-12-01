7f179b3206687e4cf9b905945d79780adf01a542
Tanisha Kapoor <tanishakapoor2020@gmail.com>
2022-12-01 14:14:38 +0530

    Create Reverse-an -array-or-string.py

---
+++ Reverse-an -array-or-string.py
@@ -0,0 +1,11 @@
+# You are given a string s. You need to reverse the string.
+def reverseWord(s):
+    #your code here
+    return(s[::-1])
+  
+if __name__ == "__main__":
+  t = int(input())
+  while(t>0):
+    s=input()
+    print(reverseWord(s))
+    t=t-1
