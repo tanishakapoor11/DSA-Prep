48ce7366cc4be79f6bde30999ee5e0461456c5e6
Tanisha Kapoor <tanishakapoor2020@gmail.com>
2023-03-07 15:33:17 +0530

    Update setMatrixZero.py

--- setMatrixZero.py
+++ setMatrixZero.py
@@ -3,28 +3,14 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        m = len(matrix)
-        n = len(matrix[0])
-        row=0
-        col=0
-        for i in range(m):
-            for j in range(n):
-                if matrix[i][j]==0:
-                    if i==0:
-                        row = 1
-                    if j==0:
-                        col =1
-                matrix[i][0]=matrix[0][j]=0
-
-        for i in range(1,m):
-            for j in range(1,n):
-                if matrix[i][0]==0 or matrix[0][j]==0:
-                    matrix[i][j]=0
-
-        if row ==1:
-            for j in range(n):
-                matrix[0][j]=0
-        if col == 1:
-            for i in range(m):
-                matrix[i][0]=0                  
+        row, col= [],[]
+        for i in range(len(matrix)):
+            for j in range(len(matrix[i])):
+                if matrix[i][j] == 0:
+                    row.append(i)
+                    col.append(j)
+        for i in range(len(matrix)):
+            for j in range(len(matrix[i])):
+                if i in row or j in col:
+                    matrix[i][j] = 0             
                        
