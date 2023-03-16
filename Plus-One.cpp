26336cff2fcbe31d65c826ef3ea520a6a112b6b8
Tanisha Kapoor <tanishakapoor2020@gmail.com>
2023-03-16 21:46:46 +0530

    Update Plus-One.cpp

--- Plus-One.cpp
+++ Plus-One.cpp
@@ -1,23 +1,19 @@
 class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
-        int n = digits.size();
-        for(int i=n-1; i>=0;i--){
-            if(i==n-1){
-                digits[i]++;
+        int n = digits.size() - 1;
+        for(int i=n; i>=0;--i){
+            if(digits[i]==9){
+                digits[i]=0;
             }
-            if(digits[i]==10){
-                digits[i]==0;
-
-                if(i!=0) {
-                digits[i-1]++;
-                }
-                else {
-                    digits.push_back(0);
-                    digits[i]=1;
-                }
+            else {
+                digits[i]+=1;
+                return digits;  
             }
         }
-        return digits;  
+        digits.push_back(0);
+        digits[0]=1;
+        return digits;
+        
     }
 };
